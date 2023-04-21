a0622d8a7e51745aed7bb631e0aac5b987635cbc
Ian Scott <scottianw@gmail.com>
2023-04-21 17:05:05 -0400

    Initial sketch for view and integration tests

--- site/tests/e2e/conftest.py
+++ site/tests/e2e/conftest.py
@@ -4,5 +4,9 @@ from invenio_app.factory import create_app as create_ui_api
 import pytest
 
 @pytest.fixture(scope='module')
+def app_config(app_config):
+    return app_config
+
+@pytest.fixture(scope='module')
 def create_app():
     return create_ui_api
---
+++ site/tests/e2e/e2e_test_factory.py
@@ -0,0 +1,15 @@
+# from pytest_invenio.fixtures import app, app_config, appctx, base_client, base_app
+from flask import url_for
+from pprint import pprint
+import sys
+
+def test_sample_e2e():
+    assert True
+
+def test_frontpage_e2e(live_server, browser):
+    # pprint(dir(base_client))
+    # pprint(dir(base_app))
+    response = browser.get(url_for('index', _external=True))
+    pprint(dir(response))
+    assert True
+
