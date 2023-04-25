360bc37db11aaa3a664602f678b18c18e6c6bbed
Ian Scott <scottianw@gmail.com>
2023-04-25 17:10:52 -0400

    Trying out React component overrides

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -7,5 +7,76 @@
 /**
  * Add here all the overridden components of your app.
  */
+// import React from "react";
+//   import { Checkbox, List, Popup, Icon } from "semantic-ui-react";
 
-export const overriddenComponents = {}
+// const MetadataOnlyToggle =  (props) => {
+//   return(
+//   <div
+//     id="ReactInvenioDeposit.FileUploaderToolbar.MetadataOnlyToggle.container"
+//     filesList={props.filesList}
+//     filesEnabled={props.filesEnabled}
+//     showMetadataOnlyToggle={props.showMetadataOnlyToggle}
+//     handleOnChangeMetadataOnly={props.handleOnChangeMetadataOnly}
+//     >
+//     {props.showMetadataOnlyToggle && (
+//         <List horizontal>
+//         <List.Item>
+//             <Checkbox
+//             toggle
+//             label={props.i18next.t("Metadata-only record switch")}
+//             onChange={props.handleOnChangeMetadataOnly}
+//             disabled={props.filesList.length > 0}
+//             checked={!props.filesEnabled}
+//             />
+//         </List.Item>
+//         <List.Item>
+//             <Popup
+//             trigger={
+//                 <Icon name="question circle outline" className="neutral" />
+//             }
+//             content={props.i18next.t("Disable files for this record")}
+//             position="top center"
+//             />
+//         </List.Item>
+//         </List>
+//     )}
+//   </div>
+//   );
+// }
+
+  import React from "react";
+  import { Checkbox } from "semantic-ui-react";
+  import { useFormikContext } from "formik";
+  import PropTypes from "prop-types";
+
+  const MetadataOnlyToggle = (props) => {
+    const { filesEnabled } = props;
+    const { setFieldValue } = useFormikContext();
+
+    const handleOnChangeMetadataOnly = () => {
+      setFieldValue("files.enabled", !filesEnabled);
+      setFieldValue("access.files", "public");
+    };
+
+    console.log('my props!!!')
+    console.log(props)
+
+    return (
+      <Checkbox
+        toggle
+        label="Metadata-only record"
+        onChange={handleOnChangeMetadataOnly}
+      />
+    );
+  };
+
+  MetadataOnlyToggle.propTypes = {
+    filesEnabled: PropTypes.bool.isRequired,
+  };
+
+export default MetadataOnlyToggle;
+
+export const overriddenComponents = {
+    "ReactInvenioDeposit.MetadataOnlyToggle.layout": MetadataOnlyToggle
+}
