8e222bbc35d7a9420523a21e02bbc739cdb61ad1
Ian Scott <scottianw@gmail.com>
2023-06-13 18:35:39 -0400

    Roughing in files for custom upload page as a blueprint

---
+++ site/knowledge_commons_repository/custom_deposits/custom_deposits.py
@@ -0,0 +1,108 @@
+"""
+form config is handled by config variables, called in invenio_app_rdm.records_ui.views.deposits.get_form_config:
+
+vocabularies=VocabulariesOptions().dump(),
+autocomplete_names=conf.get(
+    "APP_RDM_DEPOSIT_FORM_AUTOCOMPLETE_NAMES", "search"
+),
+current_locale=str(current_i18n.locale),
+default_locale=conf.get("BABEL_DEFAULT_LOCALE", "en"),
+pids=get_form_pids_config(),
+quota=conf.get("APP_RDM_DEPOSIT_FORM_QUOTA"),
+decimal_size_display=conf.get("APP_RDM_DISPLAY_DECIMAL_FILE_SIZES", True),
+user_dashboard_request=conf["RDM_REQUESTS_ROUTES"][
+    "user-dashboard-request-details"
+]
+custom_fields=custom_fields,
+publish_modal_extra=current_app.config.get(
+    "APP_RDM_DEPOSIT_FORM_PUBLISH_MODAL_EXTRA"
+),
+"""
+
+from flask import current_app, g, render_template
+from flask_login import login_required
+from invenio_app_rdm.records_ui.views.decorators import (
+    pass_draft,
+    pass_draft_community,
+    pass_draft_files
+)
+# from invenio_communities.proxies import current_communities
+from invenio_i18n import lazy_gettext as _
+# from invenio_i18n.ext import current_i18n
+# from invenio_rdm_records.proxies import current_rdm_records
+from invenio_rdm_records.resources.serializers import UIJSONSerializer
+# from invenio_rdm_records.services.schemas import RDMRecordSchema
+# from invenio_rdm_records.services.schemas.utils import dump_empty
+# from invenio_search.engine import dsl
+# from invenio_vocabularies.proxies import current_service as vocabulary_service
+# from invenio_vocabularies.records.models import VocabularyScheme
+# from invenio_vocabularies.services.custom_fields import VocabularyCF
+# from marshmallow_utils.fields.babel import gettext_from_dict
+# from sqlalchemy.orm import load_only
+
+# from ..utils import set_default_value
+from invenio_app_rdm.records_ui.views.deposits import (
+    get_search_url,
+    get_form_config,
+    get_record_permissions,
+    new_record
+)
+
+CUSTOM_DEPOSIT_FORM_TEMPLATE = \
+    "knowledge_commons_repository/records/deposit.html"
+
+
+def get_search_url():
+    """Get the search URL."""
+    # TODO: this should not be used
+    return current_app.config["APP_RDM_ROUTES"]["record_search"]
+
+
+#
+# Views
+#
+@login_required
+@pass_draft_community
+def custom_deposit_create(community=None):
+    """Create a new deposit."""
+    return render_template(
+        CUSTOM_DEPOSIT_FORM_TEMPLATE,
+        forms_config=get_form_config(createUrl="/api/records"),
+        searchbar_config=dict(searchUrl=get_search_url()),
+        record=new_record(),
+        files=dict(default_preview=None, entries=[], links={}),
+        preselectedCommunity=community,
+        permissions=get_record_permissions(
+            [
+                "manage_files",
+                "delete_draft",
+                "manage_record_access",
+            ]
+        ),
+    )
+
+
+@login_required
+@pass_draft(expand=True)
+@pass_draft_files
+def custom_deposit_edit(pid_value, draft=None, draft_files=None):
+    """Edit an existing deposit."""
+    files_dict = None if draft_files is None else draft_files.to_dict()
+    ui_serializer = UIJSONSerializer()
+    record = ui_serializer.dump_obj(draft.to_dict())
+
+    return render_template(
+        CUSTOM_DEPOSIT_FORM_TEMPLATE,
+        forms_config=get_form_config(apiUrl=f"/api/records/{pid_value}/draft"),
+        record=record,
+        files=files_dict,
+        searchbar_config=dict(searchUrl=get_search_url()),
+        permissions=draft.has_permissions_to(
+            [
+                "new_version",
+                "delete_draft",
+                "manage_files",
+                "manage_record_access",
+            ]
+        ),
+    )
--- site/knowledge_commons_repository/views.py
+++ site/knowledge_commons_repository/views.py
@@ -2,6 +2,42 @@
 
 from flask import Blueprint
 from .guides.guides import Guides
+from invenio_app_rdm.records_ui.searchapp import search_app_context
+from invenio_app_rdm.records_ui.views.filters import (
+    can_list_files,
+    compact_number,
+    get_scheme_label,
+    has_images,
+    has_previewable_files,
+    localize_number,
+    make_files_preview_compatible,
+    order_entries,
+    pid_url,
+    select_preview_file,
+    to_previewer_files,
+    truncate_number,
+)
+from invenio_app_rdm.records_ui.views.records import (
+    not_found_error,
+    record_detail,
+    record_latest,
+    record_permission_denied_error,
+    record_tombstone_error,
+)
+from invenio_pidstore.errors import (
+    PIDDeletedError,
+    PIDDoesNotExistError,
+    PIDUnregistered,
+)
+from invenio_records_resources.services.errors import (
+    FileKeyNotFoundError,
+    PermissionDeniedError,
+)
+
+from .custom_deposits.custom_deposits import (
+    custom_deposit_create,
+    custom_deposit_edit
+)
 
 #
 # Registration
@@ -14,10 +50,85 @@ def create_blueprint(app):
         template_folder="./templates",
     )
 
+    routes = app.config.get("APP_RDM_ROUTES")
+
     blueprint.add_url_rule(
         "/guides",
         view_func=Guides.as_view("support_form"),
     )
+    # Record URL rules
+    # blueprint.add_url_rule(
+    #     f'custom_{routes["record_detail"]}',
+    #     view_func=record_detail,  # "/records/<pid_value>"
+    #     # APP_RDM_RECORD_LANDING_PAGE_TEMPLATE = "invenio_app_rdm/records/detail.html"
+    # )
 
-    # Add URL rules
+    # blueprint.add_url_rule(
+    #     f'custom_{routes["record_latest"]}',  # "/records/<pid_value>/latest"
+    #     view_func=record_latest,
+    # )
+
+    # rdm_records_ext = app.extensions["invenio-rdm-records"]
+    # schemes = rdm_records_ext.records_service.config.pids_providers.keys()
+    # schemes = ",".join(schemes)
+    # if schemes:
+    #     blueprint.add_url_rule(
+    #         routes["record_from_pid"].format(schemes=schemes),
+    #         # /<any({schemes}):pid_scheme>/<path:pid_value>
+    #         view_func=record_from_pid,
+    #     )
+
+    # blueprint.add_url_rule(
+    #     routes["record_export"],  #  "/records/<pid_value>/export/<export_format>"
+    #     view_func=record_export,
+    # )
+
+    # blueprint.add_url_rule(
+    #     routes["record_file_preview"],  # /records/<pid_value>/preview/<path:filename>
+    #     view_func=record_file_preview,
+    # )
+
+    # blueprint.add_url_rule(
+    #     routes["record_file_download"],  # /records/<pid_value>/files/<path:filename>
+    #     view_func=record_file_download,
+    # )
+
+    blueprint.add_url_rule(
+        f'/custom_{routes["deposit_create"]}',  # /uploads/new
+        view_func=custom_deposit_create,
+        # APP_RDM_DEPOSIT_FORM_TEMPLATE = "invenio_app_rdm/records/deposit.html"
+    )
+
+    blueprint.add_url_rule(
+        f'/custom_{routes["deposit_edit"]}',  # /uploads/<pid_value>
+        view_func=custom_deposit_edit,
+    )
+
+    # Register error handlers
+    blueprint.register_error_handler(PIDDeletedError, record_tombstone_error)
+    blueprint.register_error_handler(PIDDoesNotExistError, not_found_error)
+    blueprint.register_error_handler(PIDUnregistered, not_found_error)
+    blueprint.register_error_handler(KeyError, not_found_error)
+    blueprint.register_error_handler(FileKeyNotFoundError, not_found_error)
+    blueprint.register_error_handler(
+        PermissionDeniedError, record_permission_denied_error
+    )
+
+    # Register template filters
+    blueprint.add_app_template_filter(can_list_files)
+    blueprint.add_app_template_filter(make_files_preview_compatible)
+    blueprint.add_app_template_filter(pid_url)
+    blueprint.add_app_template_filter(select_preview_file)
+    blueprint.add_app_template_filter(to_previewer_files)
+    blueprint.add_app_template_filter(has_previewable_files)
+    blueprint.add_app_template_filter(order_entries)
+    blueprint.add_app_template_filter(get_scheme_label)
+    blueprint.add_app_template_filter(has_images)
+    blueprint.add_app_template_filter(localize_number)
+    blueprint.add_app_template_filter(compact_number)
+    blueprint.add_app_template_filter(truncate_number)
+
+    # Register context processor
+    blueprint.app_context_processor(search_app_context)
+
     return blueprint
