1782fdc1d2ba22e85a3c08df9ea33024bb4e87e9
Ian Scott <scottianw@gmail.com>
2023-07-12 10:07:08 -0400

    Moved compound field components out of main deposit form file

--- site/knowledge_commons_repository/assets/semantic-ui/js/knowledge_commons_repository/custom_deposit/custom_RDMDepositForm.js
+++ site/knowledge_commons_repository/assets/semantic-ui/js/knowledge_commons_repository/custom_deposit/custom_RDMDepositForm.js
@@ -97,202 +97,22 @@ import { CustomFieldInjector,
          VolumeComponent,
          VersionComponent
           } from "./field_components";
+import {AdminMetadataComponent,
+        BookDetailComponent,
+        CombinedTitlesComponent,
+        TypeTitleComponent,
+        SubjectKeywordsComponent,
+        SubmissionComponent,
+        AccessRightsComponent,
+        CombinedDatesComponent,
+        DeleteComponent
+} from "./compound_field_components";
 
 
 // React Context to track the current form values.
 // Will contain the Formik values object passed up from a
 // form field.
 const FormValuesContext = createContext();
-
-const AdminMetadataComponent = ({customFieldsUI}) => {
-  return(
-    <Card fluid>
-      <Card.Content>
-        <CustomFieldInjector
-          sectionName="Commons admin info"
-          idString="AdminMetadataFields"
-          customFieldsUI={customFieldsUI}
-        />
-      </Card.Content>
-    </Card>
-)}
-
-const BookDetailComponent = ({customFieldsUI}) => {
-  return(
-    <Card fluid>
-      <Card.Content>
-        <FieldLabel htmlFor={"imprint:imprint"}
-          icon={"book"}
-          label={"Book details"}
-        />
-        <Divider fitted />
-        <Grid padded>
-          <Grid.Row>
-            <Grid.Column width="8">
-              <CustomFieldInjector
-                sectionName="Book / Report / Chapter"
-                fieldName="imprint:imprint.isbn"
-                idString="ImprintISBNField"
-                description=""
-                customFieldsUI={customFieldsUI}
-              />
-            </Grid.Column>
-            <Grid.Column width="8">
-              <VersionComponent description=""
-                label="Edition or Version"
-                icon=""
-              />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width="8">
-              <PublisherComponent />
-            </Grid.Column>
-            <Grid.Column width="8">
-              <PublicationLocationComponent customFieldsUI={customFieldsUI} />
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <VolumeComponent customFieldsUI={customFieldsUI} />
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width="8">
-              <CustomFieldInjector
-                sectionName="Book / Report / Chapter"
-                fieldName="imprint:imprint.pages"
-                idString="ImprintPagesField"
-                customFieldsUI={customFieldsUI}
-                description={""}
-                label="Number of Pages"
-              />
-            </Grid.Column>
-          </Grid.Row>
-      </Grid>
-      </Card.Content>
-    </Card>
-)}
-
-const CombinedTitlesComponent = ({vocabularies, record}) => {
-  return(
-    <Card fluid
-      id={'InvenioAppRdm.Deposit.CombinedTitlesComponent.container'}
-      as="fieldset"
-    >
-      <Card.Content>
-        <TitleComponent vocabularies={vocabularies} record={record} />
-      </Card.Content>
-    </Card>
-  )
-}
-
-const TypeTitleComponent = ({vocabularies, record}) => {
-  return(
-    <Card fluid
-      id={'InvenioAppRdm.Deposit.TypeTitleComponent.container'}
-    >
-      <Card.Content>
-        <TitleComponent vocabularies={vocabularies} record={record} />
-        <ResourceTypeComponent vocabularies={vocabularies} />
-      </Card.Content>
-    </Card>
-  )
-};
-
-const SubjectKeywordsComponent = ({ record, vocabularies, customFieldsUI }) => {
-  return(
-    <Card fluid>
-      <Card.Content>
-        <SubjectsComponent record={record} vocabularies={vocabularies} />
-        <KeywordsComponent customFieldsUI={customFieldsUI} />
-      </Card.Content>
-    </Card>
-  )
-}
-
-const SubmissionComponent = () => {
-  return(
-    <Overridable id="InvenioAppRdm.Deposit.CardDepositStatusBox.container">
-      <Card fluid>
-        <Card.Content>
-          <DepositStatusBox />
-        </Card.Content>
-        <Card.Content>
-          <Grid relaxed>
-            <Grid.Column
-              computer={8}
-              mobile={16}
-              className="pb-0 left-btn-col"
-            >
-              <SaveButton fluid />
-            </Grid.Column>
-
-            <Grid.Column
-              computer={8}
-              mobile={16}
-              className="pb-0 right-btn-col"
-            >
-              <PreviewButton fluid />
-            </Grid.Column>
-
-            <Grid.Column width={16} className="pt-10">
-              <PublishButton fluid />
-            </Grid.Column>
-          </Grid>
-        </Card.Content>
-      </Card>
-    </Overridable>
-  )
-}
-
-const AccessRightsComponent = ({ permissions }) => {
-  return(
-    <Overridable
-      id="InvenioAppRdm.Deposit.AccessRightField.container"
-      fieldPath="access"
-    >
-      <AccessRightField
-        label={i18next.t("Visibility")}
-        labelIcon="shield"
-        fieldPath="access"
-        showMetadataAccess={permissions?.can_manage_record_access}
-        fluid
-      />
-    </Overridable>
-  )
-}
-
-const CombinedDatesComponent = ({ vocabularies }) => {
-  return(
-    <Card fluid
-      id={'InvenioAppRdm.Deposit.CombinedDatesComponent.container'}
-      as="fieldset"
-    >
-      <Card.Content>
-        <DateComponent />
-        <AdditionalDatesComponent vocabularies={vocabularies} />
-      </Card.Content>
-    </Card>
-  )
-}
-
-const DeleteComponent = ({ permissions, record }) => {
-  return(
-    <>
-    {permissions?.can_delete_draft && (
-      <Overridable
-        id="InvenioAppRdm.Deposit.CardDeleteButton.container"
-        record={record}
-      >
-        <Card fluid>
-          <Card.Content>
-            <DeleteButton fluid />
-          </Card.Content>
-        </Card>
-      </Overridable>
-    )}
-    </>
-  )
-}
 
 
 const fieldComponents = {
