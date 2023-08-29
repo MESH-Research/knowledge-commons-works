77e4056ef80596f1399d72fe34912763ea0c9ed4
Ian Scott <scottianw@gmail.com>
2023-08-29 14:31:57 -0400

    Adding view for checking celery task results

---
+++ site/knowledge_commons_repository/task_results/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Knowledge Commons Repository
+# Copyright (C) 2023, MESH Research
+#
+# Knowledge Commons Repository is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+# Knowledge Commons Repository is an extended instance of InvenioRDM:
+# Copyright (C) 2019-2020 CERN.
+# Copyright (C) 2019-2020 Northwestern University.
+# Copyright (C)      2021 TU Wien.
+# Copyright (C) 2023 Graz University of Technology.
+# InvenioRDM is also free software; you can redistribute it and/or modify it
+# under the terms of the MIT License. See the LICENSE file in the
+# invenio-app-rdm package for more details.
+
+"""Knowledge Commons Repository task results module.
+
+Exposes a URL endpoint for the Knowledge Commons Repository to receive task results from the Knowledge Commons Repository's Celery workers.
+"""
