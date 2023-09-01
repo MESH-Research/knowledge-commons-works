6cf74eeeaf17f9f8864323e7d5e3432d1af37e8b
Ian Scott <scottianw@gmail.com>
2023-09-01 16:27:34 -0400

    Copied code for separate admin login from flask_security login

--- site/knowledge_commons_repository/views/admin_login/admin_login.py
+++ site/knowledge_commons_repository/views/admin_login/admin_login.py
@@ -23,20 +23,41 @@ This hidden login page is used by administrators to log in using a username and
 password. It is not linked to from any other page in the repository.
 """
 
-from flask import render_template
+from flask import (
+    render_template, current_app, request, redirect, session, after_this_request
+)
 from flask.views import MethodView
+from flask_security.utils import get_post_login_redirect, login_user
+from werkzeug.local import LocalProxy
 
+_security = LocalProxy(lambda: current_app.extensions['security'])
+
+_datastore = LocalProxy(lambda: _security.datastore)
+
+def _ctx(endpoint):
+    return _security._run_ctx_processor(endpoint)
 class AdminLogin(MethodView):
     """
     Class providing view class for administrative login.
     """
 
     def __init__(self):
-        self.template = "knowledge_commons_repository/admin_login.html"
+        self.template = "knowledge_commons_repository/view_templates/admin_login.html"
 
     def get (self):
         """
         Render the template for GET requests.
         """
+        form_class = _security.login_form
 
-        return render_template(self.template)
+        form = form_class(request.form)
+
+        if form.validate_on_submit():
+            login_user(form.user)
+            after_this_request(_datastore.commit())
+
+            return redirect(get_post_login_redirect(form.next.data))
+
+        return _security.render_template(self.template,
+                                         login_user_form=form,
+                                         **_ctx('login'))
