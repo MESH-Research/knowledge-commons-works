5522df9cb79bce595c2974f27edb0d3ba58781fd
Ian Scott <scottianw@gmail.com>
2023-11-23 14:43:00 -0500

    Removed old "guides" view and attendant files; Moved upload form framework into a separate extension; Added ui config for hclegacy fields; Moved custom deposit form config from json into config variables in invenio.cfg; Fixed AI field issue

--- site/knowledge_commons_repository/webpack.py
+++ site/knowledge_commons_repository/webpack.py
@@ -9,8 +9,6 @@ theme = WebpackThemeBundle(
     themes={
         "semantic-ui": dict(
             entry={
-                "user-guides": "./js/knowledge_commons_repository/guides.js",
-                "knowledge-commons-repository-deposit": "./js/knowledge_commons_repository/custom_deposit/index.js",
                 "custom_pdf_viewer_js": "./js/invenio_custom_pdf_viewer/pdfjs.js",
                 "custom_pdf_viewer_css": "./scss/invenio_custom_pdf_viewer/pdfjs.scss",
             },
