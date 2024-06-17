ffc7eed3e7de6bd1ada8138bb02bb1982867cffa
Ian Scott <scottianw@gmail.com>
2024-06-17 17:52:53 -0400

    feature: Added and enabled homosaurus vocab

---
+++ app_data/vocabularies/homosaurus/homosaurus_converter.py
@@ -0,0 +1,19 @@
+import json
+
+
+def convert_json(input_file, output_file):
+    with open(input_file, "r") as f:
+        data = json.load(f)
+
+    with open(output_file, "w") as f:
+        for item in data["@graph"]:
+            new_item = {
+                "id": item.get("@id", ""),
+                "scheme": "Homosaurus",
+                "subject": item.get("skos:prefLabel", ""),
+            }
+            f.write(json.dumps(new_item) + "\n")
+
+
+if __name__ == "__main__":
+    convert_json("homosaurus.v3.jsonld", "subjects_homosaurus.jsonl")
