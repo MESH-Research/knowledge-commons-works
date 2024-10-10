0bbfcb72d617e111d3637462a3322230cdb3c95a
Ian Scott <scottianw@gmail.com>
2024-10-10 12:49:33 -0400

    fix(ui, collections): Fixes to wording and layout of collection settings pages

--- assets/js/invenio_app_rdm/overridableRegistry/collections/settings/curation_policy/CurationPolicyFormLayout.js
+++ assets/js/invenio_app_rdm/overridableRegistry/collections/settings/curation_policy/CurationPolicyFormLayout.js
@@ -1,12 +1,81 @@
+/*
+* This file is part of Knowledge Commons Works.
+*   Copyright (C) 2024 Mesh Research.
+*
+* Knowledge Commons Works is based on InvenioRDM, and
+* this file is based on code from InvenioRDM. InvenioRDM is
+*   Copyright (C) 2020-2024 CERN.
+*   Copyright (C) 2020-2024 Northwestern University.
+*   Copyright (C) 2020-2024 T U Wien.
+*
+* InvenioRDM and Knowledge Commons Works are both free software;
+* you can redistribute and/or modify them under the terms of the
+* MIT License; see LICENSE file for more details.
+*/
+
 import React from "react";
 import { Header } from "semantic-ui-react";
+import { useField } from "formik";
+import { RadioField } from "react-invenio-forms";
 import { i18next } from "@translations/invenio_app_rdm/i18next";
-import { ReviewPolicyField } from "@js/invenio_communities/settings/curationPolicy/CurationPolicyForm";
+import * as Yup from "yup";
+import _get from "lodash/get";
+import PropTypes from "prop-types";
 
-const CurationPolicyFormLayout = ({ formConfig, community }) => {
+// Default set invenio_communities/views/communities.py
+const REVIEW_POLICY_FIELDS = [
+    {
+        "text": "Review all submissions",
+        "value": "closed",
+        "icon": "lock",
+        "helpText": i18next.t(
+            "All submissions to the collection must be reviewed."
+        ),
+    },
+    {
+        "text": "Allow curators, managers and owners to publish without review",
+        "value": "open",
+        "icon": "group",
+        "helpText": i18next.t(
+            "Submissions to the collection by default requires review, but curators, managers and owners can publish directly without review."
+        ),
+    },
+]
+
+const ReviewPolicyField = ({ label="", formConfig, ...props }) => {
+  const [field] = useField(props);
   return (
     <>
-      <Header as="h3">
+      {formConfig.access.review_policy.map((item) => (
+        <React.Fragment key={item.value}>
+          <RadioField
+            key={item.value}
+            fieldPath="access.review_policy"
+            label={item.text}
+            aria-label={item.text}
+            labelIcon={item.icon}
+            checked={_get(field.value, "access.review_policy") === item.value}
+            value={item.value}
+          />
+          <label className="helptext ml-20">{item.helpText}</label>
+        </React.Fragment>
+      ))}
+    </>
+  );
+};
+
+ReviewPolicyField.propTypes = {
+  label: PropTypes.string,
+  formConfig: PropTypes.object.isRequired,
+};
+
+const CurationPolicyFormLayout = ({ formConfig, community }) => {
+
+  formConfig.access.review_policy = REVIEW_POLICY_FIELDS;
+
+  return (
+    <>
+      <Header as="h3" className="mt-5">
         {i18next.t("Submission review policy")}
       </Header>
       <ReviewPolicyField formConfig={formConfig} />
@@ -14,4 +83,9 @@ const CurationPolicyFormLayout = ({ formConfig, community }) => {
   );
 };
 
-export { CurationPolicyFormLayout };
+CurationPolicyFormLayout.propTypes = {
+  formConfig: PropTypes.object.isRequired,
+  community: PropTypes.object.isRequired,
+};
+
+export { CurationPolicyFormLayout, ReviewPolicyField };
--- assets/js/invenio_app_rdm/overridableRegistry/collections/settings/privileges/CommunityPrivilegesFormLayout.js
+++ assets/js/invenio_app_rdm/overridableRegistry/collections/settings/privileges/CommunityPrivilegesFormLayout.js
@@ -1,32 +1,156 @@
-import React from "react";
-import { MembersVisibilityField, VisibilityField } from "@js/invenio_communities/settings/privileges/CommunityPriviledgesForm";
-import { Header } from "semantic-ui-react";
-import { i18next } from "@translations/invenio_app_rdm/i18next";
+/*
+* This file is part of Knowledge Commons Works.
+*   Copyright (C) 2024 Mesh Research.
+*
+* Knowledge Commons Works is based on InvenioRDM, and
+* this file is based on code from InvenioRDM. InvenioRDM is
+*   Copyright (C) 2020-2024 CERN.
+*   Copyright (C) 2020-2024 Northwestern University.
+*   Copyright (C) 2020-2024 T U Wien.
+*
+* InvenioRDM and Knowledge Commons Works are both free software;
+* you can redistribute and/or modify them under the terms of the
+* MIT License; see LICENSE file for more details.
+*/
 
-const CommunityPrivilegesFormLayout = ({ formConfig, community }) => {
+import React from "react";
+// import { MembersVisibilityField, VisibilityField } from "@js/invenio_communities/settings/privileges/CommunityPriviledgesForm";
+import { useField } from "formik";
+import { Header } from "semantic-ui-react";
+import { RadioField } from "react-invenio-forms";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+import PropTypes from "prop-types";
+import _get from "lodash/get";
+
+// Default set invenio_communities/views/communities.py
+const VISIBILITY_FIELDS = [
+  {
+    text: "Public",
+    value: "public",
+    icon: "group",
+    helpText: i18next.t(
+      "Your collection is publicly accessible" +
+        " and shows up in search results."
+    ),
+  },
+  {
+    text: "Restricted",
+    value: "restricted",
+    icon: "lock",
+    helpText: i18next.t(
+      "Your collection is restricted to users" + " with access."
+    ),
+  },
+];
+
+// Default set invenio_communities/views/communities.py
+const MEMBERS_VISIBILITY_FIELDS = [
+  {
+    text: "Public",
+    value: "public",
+    icon: "group",
+    helpText: i18next.t(
+      "Members who have set their visibility to public are visible " +
+        "to anyone. Members with hidden visibility are only visible " +
+        "to other members."
+    ),
+  },
+  {
+    text: "Members-only",
+    value: "restricted",
+    icon: "lock",
+    helpText: i18next.t(
+      "Members in your community are only visible to other members of " +
+        "the community."
+    ),
+  },
+];
+
+const VisibilityField = ({ label, formConfig, ...props }) => {
+  const [field] = useField(props);
   return (
     <>
-    <Header as="h3">
-        {i18next.t("Community visibility")}
-        <Header.Subheader className="mt-5">
-        {i18next.t(
-            "Controls if the community is visible to anyone or to members only."
-        )}
-        </Header.Subheader>
-    </Header>
-    <VisibilityField formConfig={formConfig} />
+      {formConfig.access.visibility.map((item) => (
+        <React.Fragment key={item.value}>
+          <RadioField
+            key={item.value}
+            fieldPath="access.visibility"
+            label={item.text}
+            labelIcon={item.icon}
+            checked={_get(field.value, "access.visibility") === item.value}
+            value={item.value}
+          />
+          <label className="helptext ml-15">{item.helpText}</label>
+        </React.Fragment>
+      ))}
+    </>
+  );
+};
 
-    <Header as="h3">
+VisibilityField.propTypes = {
+  label: PropTypes.string,
+  formConfig: PropTypes.object.isRequired,
+};
+
+VisibilityField.defaultProps = {
+  label: "",
+};
+
+const MembersVisibilityField = ({ label = "", formConfig, ...props }) => {
+  const [field] = useField(props);
+  return (
+    <>
+      {formConfig.access.members_visibility.map((item) => (
+        <React.Fragment key={item.value}>
+          <RadioField
+            key={item.value}
+            fieldPath="access.members_visibility"
+            label={item.text}
+            labelIcon={item.icon}
+            checked={
+              _get(field.value, "access.members_visibility") === item.value
+            }
+            value={item.value}
+          />
+          <label className="helptext ml-15">{item.helpText}</label>
+        </React.Fragment>
+      ))}
+    </>
+  );
+};
+
+MembersVisibilityField.propTypes = {
+  label: PropTypes.string,
+  formConfig: PropTypes.object.isRequired,
+};
+
+const CommunityPrivilegesFormLayout = ({ formConfig, community }) => {
+  formConfig.access.visibility = VISIBILITY_FIELDS;
+  formConfig.access.members_visibility = MEMBERS_VISIBILITY_FIELDS;
+
+  return (
+    <>
+      <Header as="h3" className="mt-5">
+        {i18next.t("Collection visibility")}
+        <Header.Subheader className="mt-5">
+          {i18next.t(
+            "Controls whether the collection is visible to anyone or to members only."
+          )}
+        </Header.Subheader>
+      </Header>
+      <VisibilityField formConfig={formConfig} />
+
+      <Header as="h3" className="mt-5">
         {i18next.t("Members visibility")}
         <Header.Subheader className="mt-5">
-        {i18next.t(
+          {i18next.t(
             "Controls whether the members tab is visible to anyone or to members only."
-        )}
+          )}
         </Header.Subheader>
-    </Header>
-    <MembersVisibilityField formConfig={formConfig} />
-    {/* TODO: Re-enable once properly integrated to be displayed */}
-    {/*
+      </Header>
+      <MembersVisibilityField formConfig={formConfig} />
+      {/* TODO: Re-enable once properly integrated to be displayed */}
+      {/*
             <Grid.Column width={6}>
             <Header as="h3">Records permissions</Header>
             <p>This is a text explaining about the permission</p>
@@ -55,4 +179,8 @@ const CommunityPrivilegesFormLayout = ({ formConfig, community }) => {
   );
 };
 
-export { CommunityPrivilegesFormLayout };
+export {
+  CommunityPrivilegesFormLayout,
+  VisibilityField,
+  MembersVisibilityField,
+};
