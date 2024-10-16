5bf309e496f153259b5375e224c4bbe993b40be0
Ian Scott <scottianw@gmail.com>
2024-10-15 20:11:52 -0400

    fix(tests): Improved tests for basic draft creation and publication

--- site/tests/api/test_api_record_ops.py
+++ site/tests/api/test_api_record_ops.py
@@ -1,13 +1,12 @@
 import json
 import pytest
-from pprint import pprint
 import re
 
 import arrow
 
 links_template = {
     "self": "{0}/records/{1}/draft",
-    "self_html": "{0}/uploads/{1}",
+    "self_html": "{2}/uploads/{1}",
     "self_iiif_manifest": "{0}/iiif/draft:{1}/manifest",
     "self_iiif_sequence": "{0}/iiif/draft:{1}/sequence/default",
     "files": "{0}/records/{1}/draft/files",
@@ -15,7 +14,7 @@ links_template = {
     "archive": "{0}/records/{1}/draft/files-archive",
     "archive_media": "{0}/records/{1}/draft/media-files-archive",
     "record": "{0}/records/{1}",
-    "record_html": "{0}/records/{1}",
+    "record_html": "{2}/records/{1}",
     "publish": "{0}/records/{1}/draft/actions/publish",
     "review": "{0}/records/{1}/draft/review",
     "versions": "{0}/records/{1}/versions",
@@ -34,6 +33,7 @@ links_template = {
 
 def test_draft_creation(
     running_app,
+    db,
     user_factory,
     client_with_login,
     minimal_record,
@@ -61,7 +61,7 @@ def test_draft_creation(
     with app.test_client() as client:
         logged_in_client, _ = client_with_login(client, user)
         response = logged_in_client.post(
-            f"{app.config['SITE_API_URL']}/api/records",
+            f"{app.config['SITE_API_URL']}/records",
             data=json.dumps(minimal_record),
             headers={**headers, "Authorization": f"Bearer {token}"},
         )
@@ -86,9 +86,10 @@ def test_draft_creation(
             == actual_draft["updated"]
         )
 
-        test_url = app.config["SITE_UI_URL"]
+        test_api_url = app.config["SITE_API_URL"]
+        test_ui_url = app.config["SITE_UI_URL"]
         assert actual_draft["links"] == {
-            k: v.format(test_url, actual_draft_id)
+            k: v.format(test_api_url, actual_draft_id, test_ui_url)
             for k, v in links_template.items()
         }
 
@@ -221,6 +222,137 @@ def test_draft_creation(
         assert actual_draft["ui"]["version"] == "v1"
         assert actual_draft["ui"]["is_draft"]
 
+        # publish the record
+        publish_response = logged_in_client.post(
+            f"{app.config['SITE_API_URL']}/records/{actual_draft_id}/draft"
+            "/actions/publish",
+            headers={**headers, "Authorization": f"Bearer {token}"},
+        )
+        assert publish_response.status_code == 202
+
+        actual_published = publish_response.json
+        assert actual_published["id"] == actual_draft_id
+        assert actual_published["is_published"]
+        assert not actual_published["is_draft"]
+        assert actual_published["revision_id"] == 3
+        assert actual_published["versions"]["is_latest"]
+        assert actual_published["versions"]["is_latest_draft"]
+        assert actual_published["versions"]["index"] == 1
+        assert actual_published["status"] == "published"
+        assert actual_published["ui"]["version"] == "v1"
+        assert not actual_published["ui"]["is_draft"]
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_record_publication(
+    running_app,
+    db,
+    client_with_login,
+    minimal_record,
+    headers,
+    user_factory,
+    search_clear,
+):
+
+    app = running_app.app
+
+    u = user_factory(
+        email="test@example.com",
+        password="test",
+        token=True,
+        admin=True,
+        saml_src="knowledgeCommons",
+        saml_id="user1",
+    )
+    user = u.user
+    print(user)
+    # identity = u.identity
+    # print(identity)
+    token = u.allowed_token
+    print(token)
+
+    with app.test_client() as client:
+        logged_in_client, _ = client_with_login(client, user)
+        response = logged_in_client.post(
+            f"{app.config['SITE_API_URL']}/records",
+            data=json.dumps(minimal_record),
+            headers={**headers, "Authorization": f"Bearer {token}"},
+        )
+        assert response.status_code == 201
+
+        actual_draft = response.json
+        actual_draft_id = actual_draft["id"]
+
+        publish_response = logged_in_client.post(
+            f"{app.config['SITE_API_URL']}/records/{actual_draft_id}/draft"
+            "/actions/publish",
+            headers={**headers, "Authorization": f"Bearer {token}"},
+        )
+        assert publish_response.status_code == 202
+
+        actual_published = publish_response.json
+        assert actual_published["id"] == actual_draft_id
+        assert actual_published["is_published"]
+        assert actual_published["versions"]["is_latest"]
+        assert actual_published["versions"]["is_latest_draft"] is False
+        assert actual_published["versions"]["index"] == 2
+        assert actual_published["status"] == "published"
+        assert actual_published["ui"]["is_published"]
+        assert actual_published["ui"]["version"] == "v2"
+        assert not actual_published["ui"]["is_draft"]
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_record_draft_update(
+    running_app,
+    db,
+    client_with_login,
+    minimal_record,
+    headers,
+    user_factory,
+    search_clear,
+):
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_record_published_update(
+    running_app,
+    db,
+    client_with_login,
+    minimal_record,
+    headers,
+    user_factory,
+    search_clear,
+):
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_record_versioning(
+    running_app,
+    db,
+    client_with_login,
+    minimal_record,
+    headers,
+    user_factory,
+    search_clear,
+):
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_record_file_upload(
+    running_app,
+    db,
+    client_with_login,
+    minimal_record,
+    headers,
+    user_factory,
+    search_clear,
+):
+    pass
+
 
 @pytest.mark.skip(reason="Not implemented")
 def test_db(running_app, client):
@@ -233,179 +365,4 @@ def test_db(running_app, client):
         "status": 404,
     }
 
-    # res2 = client.get('/api/records/jznz9-qhx89')
-    # pprint(dir(res2))
-    # assert res2 == {}
-
     assert True
-
-
-# dir(response from client.get)
-# ['__annotations__',
-#  '__call__',
-#  '__class__',
-#  '__delattr__',
-#  '__dict__',
-#  '__dir__',
-#  '__doc__',
-#  '__enter__',
-#  '__eq__',
-#  '__exit__',
-#  '__format__',
-#  '__ge__',
-#  '__getattribute__',
-#  '__gt__',
-#  '__hash__',
-#  '__init__',
-#  '__init_subclass__',
-#  '__le__',
-#  '__lt__',
-#  '__module__',
-#  '__ne__',
-#  '__new__',
-#  '__reduce__',
-#  '__reduce_ex__',
-#  '__repr__',
-#  '__setattr__',
-#  '__sizeof__',
-#  '__str__',
-#  '__subclasshook__',
-#  '__test__',
-#  '__weakref__',
-#  '_clean_status',
-#  '_compat_tuple',
-#  '_ensure_sequence',
-#  '_is_range_request_processable',
-#  '_on_close',
-#  '_process_range_request',
-#  '_status',
-#  '_status_code',
-#  '_wrap_range_response',
-#  'accept_ranges',
-#  'access_control_allow_credentials',
-#  'access_control_allow_headers',
-#  'access_control_allow_methods',
-#  'access_control_allow_origin',
-#  'access_control_expose_headers',
-#  'access_control_max_age',
-#  'add_etag',
-#  'age',
-#  'allow',
-#  'autocorrect_location_header',
-#  'automatically_set_content_length',
-#  'cache_control',
-#  'calculate_content_length',
-#  'call_on_close',
-#  'charset',
-#  'close',
-#  'content_encoding',
-#  'content_language',
-#  'content_length',
-#  'content_location',
-#  'content_md5',
-#  'content_range',
-#  'content_security_policy',
-#  'content_security_policy_report_only',
-#  'content_type',
-#  'cross_origin_embedder_policy',
-#  'cross_origin_opener_policy',
-#  'data',
-#  'date',
-#  'default_mimetype',
-#  'default_status',
-#  'delete_cookie',
-#  'direct_passthrough',
-#  'expires',
-#  'force_type',
-#  'freeze',
-#  'from_app',
-#  'get_app_iter',
-#  'get_data',
-#  'get_etag',
-#  'get_json',
-#  'get_wsgi_headers',
-#  'get_wsgi_response',
-#  'headers',
-#  'history',
-#  'implicit_sequence_conversion',
-#  'is_json',
-#  'is_sequence',
-#  'is_streamed',
-#  'iter_encoded',
-#  'json',
-#  'json_module',
-#  'last_modified',
-#  'location',
-#  'make_conditional',
-#  'make_sequence',
-#  'max_cookie_size',
-#  'mimetype',
-#  'mimetype_params',
-#  'request',
-#  'response',
-#  'retry_after',
-#  'set_cookie',
-#  'set_data',
-#  'set_etag',
-#  'status',
-#  'status_code',
-#  'stream',
-#  'text',
-#  'vary',
-#  'www_authenticate']
-
-
-# dir(client)
-# '__annotations__',
-#  '__class__',
-#  '__delattr__',
-#  '__dict__',
-#  '__dir__',
-#  '__doc__',
-#  '__enter__',
-#  '__eq__',
-#  '__exit__',
-#  '__format__',
-#  '__ge__',
-#  '__getattribute__',
-#  '__gt__',
-#  '__hash__',
-#  '__init__',
-#  '__init_subclass__',
-#  '__le__',
-#  '__lt__',
-#  '__module__',
-#  '__ne__',
-#  '__new__',
-#  '__reduce__',
-#  '__reduce_ex__',
-#  '__repr__',
-#  '__setattr__',
-#  '__sizeof__',
-#  '__str__',
-#  '__subclasshook__',
-#  '__weakref__',
-#  '_context_stack',
-#  '_copy_environ',
-#  '_new_contexts',
-#  '_request_from_builder_args',
-#  'allow_subdomain_redirects',
-#  'application',
-#  'cookie_jar',
-#  'delete',
-#  'delete_cookie',
-#  'environ_base',
-#  'get',
-#  'head',
-#  'open',
-#  'options',
-#  'patch',
-#  'post',
-#  'preserve_context',
-#  'put',
-#  'resolve_redirect',
-#  'response_wrapper',
-#  'run_wsgi_app',
-#  'session_transaction',
-#  'set_cookie',
-#  'trace']
