245845afe1ab649021ad45c4f6ecc2234792fe20
Martin Paul Eve <martin@eve.gd>
2025-06-17 17:14:05 +0100

    feat(oauth): further work on new oAuth system

--- site/kcworks/ext.py
+++ site/kcworks/ext.py
@@ -9,9 +9,13 @@
 Initialize the main KCWorks extension object along with its services, blueprints,
 and components.
 """
+from threading import Lock
 
 from flask import Flask
 from invenio_rdm_records.services.components import DefaultRecordsComponents
+from werkzeug.routing import Rule
+
+from kcworks.services.cilogon import cilogon_service
 from kcworks.services.notifications.service import (
     InternalNotificationService,
     InternalNotificationServiceConfig,
@@ -26,6 +30,14 @@ from kcworks.services.records.record_communities.community_change_permissions_co
     CommunityChangePermissionsComponent,
 )
 from kcworks.templates.template_filters import user_profile_dict
+
+_first_request_done = False
+_setup_lock = Lock()
+
+ROUTE_REWRITES = {
+    "/oauth/login/<remote_app>/": cilogon_service.login,
+    "/oauth/authorized/<remote_app>/": cilogon_service.authorized,
+}
 
 
 class KCWorks:
@@ -53,6 +65,34 @@ class KCWorks:
         self.init_components(app)
         self.init_template_filters(app)
         app.extensions["kcworks"] = self
+
+        @app.before_request
+        def route_rewriter():
+            global _first_request_done
+
+            if _first_request_done:
+                return
+
+            with _setup_lock:
+                if _first_request_done:  # double check after lock
+                    return
+
+                try:
+                    for rule in app.url_map.iter_rules():
+                        route_str = str(rule)
+
+                        if route_str in ROUTE_REWRITES:
+                            if rule.endpoint in app.view_functions:
+                                app.view_functions[rule.endpoint] = (
+                                    ROUTE_REWRITES[route_str]
+                                )
+                            else:
+                                app.logger.debug(
+                                    f"Warning: Endpoint '{rule.endpoint}' not "
+                                    f"found for route '{route_str}'"
+                                )
+                finally:
+                    _first_request_done = True
 
     def init_services(self, app: Flask) -> None:
         """Initialize services for the KCWorks extension.
