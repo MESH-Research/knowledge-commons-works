cd1bffa235cfe24af4f87ec4d02551a4a9e7e357
Ian Scott <scottianw@gmail.com>
2025-08-12 13:38:53 -0400

    fix(testing): Adding automated isort run to test runner

--- run-tests.sh
+++ run-tests.sh
@@ -54,6 +54,7 @@ function check_docker_compose_running() {
 # Note: "-k" would clash with "pytest"
 keep_services=0
 skip_translations=0
+isort=0
 pytest_args=()
 for arg in $@; do
 	# from the CLI args, filter out some known values and forward the rest to "pytest"
@@ -65,6 +66,9 @@ for arg in $@; do
 			;;
 		-S|--skip-translations)
 			skip_translations=1
+			;;
+		-I|--isort)
+			isort=1
 			;;
 		*)
 			pytest_args+=( ${arg} )
@@ -108,17 +112,19 @@ echo "Running mypy on the site directory"
 uv run mypy --config-file pyproject.toml site/
 
 # Run isort on default paths
-echo "Running isort on site/kcworks, tests, and scripts directories"
-uv run isort site/kcworks tests scripts
+if [[ ${isort} -eq 1 ]]; then
+	echo "Running isort on site/kcworks, tests, and scripts directories"
+	uv run isort site/kcworks tests scripts
+fi
 
 # Note: expansion of pytest_args looks like below to not cause an unbound
 # variable error when 1) "nounset" and 2) the array is empty.
 if [ ${#pytest_args[@]} -eq 0 ]; then
 	echo "Running pytest"
-	uv run --env-file tests/.env python -m pytest -vv --disable-warnings
+	uv run --env-file tests/.env python -m pytest -vv -s --disable-warnings
 else
 	echo "Running pytest with additional arguments"
-	uv run --env-file tests/.env python -m pytest ${pytest_args[@]} --disable-warnings
+	uv run --env-file tests/.env python -m pytest ${pytest_args[@]} -s --disable-warnings
 fi
 
 tests_exit_code=$?
