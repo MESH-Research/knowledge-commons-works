a33f78cd255f9484623d731a41aa8bbd6e6fd190
Ian W. Scott <scottianw@gmail.com>
2026-07-01 15:01:49 -0400

    feat(scheduler): Add scheduler startup script for staging/prod.
    - Also includes updates/cleanups to the other container startup scripts.

--- docker/startup_worker.sh
+++ docker/startup_worker.sh
@@ -5,12 +5,6 @@
 cd /opt/invenio/src
 source /opt/invenio/src/.venv/bin/activate
 
-# Remove existing SAML certificate files
-rm -f /opt/invenio/var/instance/saml.crt /opt/invenio/var/instance/saml.key
-
-# Write SAML certificate files from environment variables
-echo "$SAML_CERT" > /opt/invenio/var/instance/saml.crt
-echo "$SAML_KEY" > /opt/invenio/var/instance/saml.key
-
 # Start celery worker
 exec /opt/invenio/src/.venv/bin/python -m celery -A invenio_app.celery worker --beat --loglevel INFO
+
