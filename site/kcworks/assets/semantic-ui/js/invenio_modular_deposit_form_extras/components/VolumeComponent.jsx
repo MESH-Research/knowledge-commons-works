d51b2d68783eb0c702c3d5a38f99cf69faeec99c
Ian W. Scott <scottianw@gmail.com>
2026-08-01 00:56:32 -0400

    fix(upload-form): Separate volume subfields into independent components

--- site/kcworks/assets/semantic-ui/js/invenio_modular_deposit_form_extras/components/VolumeComponent.jsx
+++ site/kcworks/assets/semantic-ui/js/invenio_modular_deposit_form_extras/components/VolumeComponent.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { i18next } from "@translations/kcworks/i18next";
 import { CustomField } from "@js/invenio_modular_deposit_form/field_components/CustomField";
 
 const VolumeComponent = ({ ...extraProps }) => (
   <CustomField
-    fieldName="kcr:volumes"
-    idString="KcrVolumes"
+    fieldName="kcr:volumes.volume"
+    idString="KcrVolumeField"
+    label={i18next.t("Volume")}
+    description=""
+    icon="book"
     {...extraProps}
   />
 );
